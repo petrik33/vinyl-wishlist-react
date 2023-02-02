328adbc028b2ed3d3c2bf5b5ca29f63a969420ca
Timofey Petrikevich <petrik3003@gmail.com>
2023-02-03 02:16:13 +0300

    feat: upload images

---
+++ src/Data.tsx
@@ -0,0 +1,48 @@
+const AUTHORS_NAMES = [
+    'Bowie',
+    'АукцЫон',
+    'Led Zeppelin',
+    'Мумий Тролль',
+    'Cure',
+    'Doors',
+    'Iggy Pop',
+    'Lou Reed',
+    'Tom Waits',
+    'Talking Heads'
+];
+
+const ALBUMS_NAMES = [
+    'Scary Monsters and Super Creeps',
+    'Low',
+    'The Next Day',
+    'Птица',
+    'Бодун',
+    'II',
+    'Морская',
+    'Икра',
+    'Three Imaginary Boys',
+    'Seventeen Seconds',
+    'Strange Days',
+    'Lust for Life',
+    'Transformer',
+    'Bone Machine',
+    'Talking Heads 77'
+];
+
+const ALBUMS_COVER_SRC = [
+    './Covers/R-423056-1479638131-8059.jpg',
+    './Covers/R-216166-1275625649.jpg',
+    './Covers/TheNextDay.jpg',
+    './Covers/Ptiza.jpg',
+    './Covers/Bodun.jpg',
+    './Covers/LedII.jpg',
+    './Covers/Morskaya.jpg',
+    './Covers/Ikra.jpg',
+    './Covers/TIB.jpg',
+    './Covers/SecondsCure.jpg',
+    './Covers/StrangeDays.jpg',
+    './Covers/LustForLife.jpg',
+    './Covers/Transformer.jpg',
+    './Covers/BoneMachine.jpg',
+    './Covers/TalkingHeads.jpg'
+];
