3dc8ac2e7535c1e6c3000e2f850f1f9af3a270b8
Timofey Petrikevich <petrik3003@gmail.com>
2023-02-03 20:35:13 +0300

    feat: web image component

---
+++ src/Components/Utilities/WebImage.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+export interface IWebImageProps {
+    sources?: Array<JSX.Element>;
+    srcSet?: string;
+    src: string;
+    sizes?: string;
+    alt: string;
+}
+
+export function WebImage (props: IWebImageProps) {
+  return (
+    <picture>
+        {props?.sources}
+        <img
+            src={props.src}
+            srcSet={props.srcSet}
+            sizes={props.sizes}
+            alt={props.alt}
+        />
+    </picture>
+  );
+}
