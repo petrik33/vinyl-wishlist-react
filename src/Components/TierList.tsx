bfeea6d28723bd044b4db5785906ee973a3627e5
Timofey Petrikevich <petrik3003@gmail.com>
2023-02-08 23:14:46 +0300

    feat: Mode Choice

--- src/Components/TierList.tsx
+++ src/Components/TierList.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import GroupView, { IGroupViewProps } from './GroupView';
-import AlbumModalView from './AlbumModalView';
 import GroupViewProvider from './GroupViewProvider';
+import EditModeProvider from '../Context/EditingModeContext';
+import TierListModeButtonsGroup from './TierListModeButtonsGroup';
 
 export interface ITierListProps {
 }
 
 export default function TierList (props: ITierListProps) {
-
     return (
         <div className='tier-list'>
-            <GroupViewProvider />
+            <EditModeProvider>
+                <h1 className='tier-list-header'>Tier List</h1>
+                <TierListModeButtonsGroup />
+                <GroupViewProvider />
+            </EditModeProvider>
         </div>
     );
 }
