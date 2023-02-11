724d52ffe9d91714e7a3304b8cacead9e5dd2590
Timofey Petrikevich <petrik3003@gmail.com>
2023-02-11 22:21:51 +0300

    utility: filterWithId

--- src/Components/Utilities/CodeUtilities.tsx
+++ src/Components/Utilities/CodeUtilities.tsx
@@ -1,5 +1,11 @@
-export const filterId = <T extends { id: string}>(array: Array<T>, id: string)  : Array<T> => {
+export const filterWithId = <T extends { id: string}>(array: Array<T>, id: string)  : Array<T> => {
     return array.filter((value) => {
         return (value.id !== id);
+    })
+}
+
+export const filterId = (array: Array<string>, id: string) => {
+    return array.filter((value) => {
+        return (value !== id);
     })
 }
