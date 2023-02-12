155c72280f4208004f744694c8aef80ceddec053
Timofey Petrikevich <petrik3003@gmail.com>
2023-02-12 21:43:52 +0300

    misc: identation fix

--- src/Context/TierGroupsContext.tsx
+++ src/Context/TierGroupsContext.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createContext, useContext } from 'react';
-import { AlbumsData, IAlbum, IAlbumsData } from '../Data/Data';
+import { AlbumsData, IAlbumsData } from '../Data/Data';
 import { filterId } from '../Utilities/CodeUtilities';
 import { ImmerReducer, useImmerReducer } from 'use-immer';
 import { Immutable } from 'immer';
@@ -10,22 +10,22 @@ export const TierNames = ['S', 'A', 'B', 'C', 'D'] as const;
 
 export type TierName = typeof TierNames[number];
 export type TierGroup = {
-    name: string,
-    id: TierName,
-    albums: string[]
+  name: string,
+  id: TierName,
+  albums: string[]
 }
 export type TierGroups = {
-    [key in TierName]: TierGroup;
+  [key in TierName]: TierGroup;
 }
 export type TierGroupsState = Immutable<TierGroups>;
 export type TierGroupsDispatch = React.Dispatch<TierGroupsAction>;
 
 const TierGroupsContext = createContext<TierGroupsState>({} as TierGroupsState);
 const TierGroupsDispatchContext = createContext<TierGroupsDispatch>(
-    {} as TierGroupsDispatch);
+  {} as TierGroupsDispatch);
 
 export interface IProviderProps {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 export function TierGroupsProvider(props: IProviderProps) {
@@ -48,56 +48,56 @@ export function useTierGroups() {
 }
 
 export function useTierGroupsDispatch() {
-    return useContext(TierGroupsDispatchContext);
+  return useContext(TierGroupsDispatchContext);
 }
 
 export enum TierGroupsActionKind {
-    MOVE_ALBUM = 'move-album',
-    DELETE_ALBUM = 'delete-album',
-    RESET_TO_DEBUG = 'reset-to-debug'
+  MOVE_ALBUM = 'move-album',
+  DELETE_ALBUM = 'delete-album',
+  RESET_TO_DEBUG = 'reset-to-debug'
 };
 export type TierGroupsActionMove = {
-    type: TierGroupsActionKind.MOVE_ALBUM;
-    id: string;
-    from: TierName;
-    to: TierName;
+  type: TierGroupsActionKind.MOVE_ALBUM;
+  id: string;
+  from: TierName;
+  to: TierName;
 }
 export type TierGroupsActionDelete = {
-    type: TierGroupsActionKind.DELETE_ALBUM;
-    id: string;
-    from: TierName;
+  type: TierGroupsActionKind.DELETE_ALBUM;
+  id: string;
+  from: TierName;
 }
 export type TierGroupsActionResetToDebug = {
-    type: TierGroupsActionKind.RESET_TO_DEBUG;
-    parts: number;
+  type: TierGroupsActionKind.RESET_TO_DEBUG;
+  parts: number;
 }
 export type TierGroupsAction = 
-    | TierGroupsActionMove
-    | TierGroupsActionDelete
-    | TierGroupsActionResetToDebug
+  | TierGroupsActionMove
+  | TierGroupsActionDelete
+  | TierGroupsActionResetToDebug
 
 const TierGroupsReducer : ImmerReducer<TierGroupsState, TierGroupsAction> = (tierGroupsDraft, action) => {
-    switch (action.type) {
-        case TierGroupsActionKind.MOVE_ALBUM: {
-            return TierGroupsMoveAlbum(tierGroupsDraft, action);
-        }
+	switch (action.type) {
+		case TierGroupsActionKind.MOVE_ALBUM: {
+			return TierGroupsMoveAlbum(tierGroupsDraft, action);
+		}
 
-        case TierGroupsActionKind.DELETE_ALBUM: {
-            return TierGroupsDeleteAlbum(tierGroupsDraft, action);
-        }
+		case TierGroupsActionKind.DELETE_ALBUM: {
+			return TierGroupsDeleteAlbum(tierGroupsDraft, action);
+		}
 
-        case TierGroupsActionKind.RESET_TO_DEBUG: {
-            return getDebugTierGroups(AlbumsData, action.parts);
-        }
+		case TierGroupsActionKind.RESET_TO_DEBUG: {
+			return getDebugTierGroups(AlbumsData, action.parts);
+		}
 
-        default: {
-            throw Error('Unknown action');
-        }
-    }
+		default: {
+			throw Error('Unknown action');
+		}
+	}
 }
 
 function TierGroupsDeleteAlbum(tierGroupsDraft: Draft<TierGroupsState>,
-action : TierGroupsActionDelete) : TierGroupsState {
+	action : TierGroupsActionDelete) : TierGroupsState {
     const fromAlbums = tierGroupsDraft[action.from].albums;
     const id = action.id;
     
@@ -107,60 +107,60 @@ action : TierGroupsActionDelete) : TierGroupsState {
 }
 
 function TierGroupsMoveAlbum (tierGroupsDraft: Draft<TierGroupsState>,
-action: TierGroupsActionMove) : TierGroupsState {
-    const fromAlbums = tierGroupsDraft[action.from].albums;
-    const toAlbums = tierGroupsDraft[action.to].albums;
-    const id = action.id;
+	action: TierGroupsActionMove) : TierGroupsState {
+		const fromAlbums = tierGroupsDraft[action.from].albums;
+		const toAlbums = tierGroupsDraft[action.to].albums;
+		const id = action.id;
 
-    tierGroupsDraft[action.from].albums = filterId(fromAlbums, id);
-    toAlbums.push(id);
+		tierGroupsDraft[action.from].albums = filterId(fromAlbums, id);
+		toAlbums.push(id);
 
-    return tierGroupsDraft;
+		return tierGroupsDraft;
+}
+
+export const InitialTierGroups : TierGroups = {
+	'S' : {
+		name: 'S',
+		id: 'S',
+		albums: []
+	},
+	'A' : {
+		name: 'A',
+		id: 'A',
+		albums: []
+	},
+	'B' : {
+		name: 'B',
+		id: 'B',
+		albums: []
+	},
+	'C' : {
+		name: 'C',
+		id: 'C',
+		albums: []
+	},
+	'D' : {
+		name: 'D',
+		id: 'D',
+		albums: []
+	}
 }
 
 //Debug
 function getDebugTierGroups(data: IAlbumsData, parts: number) : TierGroups {
-    const albumsNum = Object.keys(data).length;
-    const minPerGroup = Math.floor(albumsNum / parts);
+  const albumsNum = Object.keys(data).length;
+  const minPerGroup = Math.floor(albumsNum / parts);
 
-    let tierGroups: TierGroups = InitialTierGroups;
+  let tierGroups: TierGroups = InitialTierGroups;
 
-    let counter = 0;
-    for(const albumId in data) {
-        const tierIdx = Math.floor(counter / minPerGroup);
-        const tier = TierNames[tierIdx];
-        tierGroups[tier].albums.push(albumId);
-    }
+  let counter = 0;
+  for(const albumId in data) {
+      const tierIdx = Math.floor(counter / minPerGroup);
+      const tier = TierNames[tierIdx];
+      tierGroups[tier].albums.push(albumId);
+  }
 
-    return tierGroups;
-}
-
-export const InitialTierGroups : TierGroups = {
-    'S' : {
-        name: 'S',
-        id: 'S',
-        albums: []
-    },
-    'A' : {
-        name: 'A',
-        id: 'A',
-        albums: []
-    },
-    'B' : {
-        name: 'B',
-        id: 'B',
-        albums: []
-    },
-    'C' : {
-        name: 'C',
-        id: 'C',
-        albums: []
-    },
-    'D' : {
-        name: 'D',
-        id: 'D',
-        albums: []
-    }
+  return tierGroups;
 }
 
 
