452e3f961f63249e07901f1c6f72affccc55d697
Timofey Petrikevich <petrik3003@gmail.com>
2023-02-19 21:12:49 +0300

    misc: unnecesary css import

---
+++ src/Components/ToolbarButtons/ToolbarButtons.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export interface IToolbarButtonsProps {
+  
+}
+
+const ToolbarButtons : React.FC<IToolbarButtonsProps> = (props) => {
+  return (
+    <div >
+      
+    </div>
+  );
+}
+
+export default ToolbarButtons;
