fc89d3753f79b890d24106712cb7e6df68cdcb55
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-03-05 01:00:51 +0300

    feat: album data interface separate

--- src/Data/Data.ts
+++ src/Data/Data.ts
@@ -26,8 +26,9 @@ export interface IAlbumData {
   height?: number | string;
 }
 
-export interface IAlbum extends IAlbumData {
+export interface IAlbum {
   id: string;
+  data: IAlbumData;
 }
 
 export interface IAuthorData {
@@ -43,64 +44,76 @@ export interface IAlbumsCollectionData {
 }
 
 export const AlbumsDebugData : IAlbumsCollectionData = {
-  'album-1123123': {
-      id: 'album-1123123',
-      name: 'Scary Monsters and Super Creeps',
-      src: 'https://upload.wikimedia.org/wikipedia/ru/0/0e/DavidBowieScaryMonstersCover.jpg',
-  },
-  'album-212312': {
-      id: 'album-212312',
-      name: 'The Next Day',
-      src: 'https://upload.wikimedia.org/wikipedia/ru/0/0e/DavidBowieScaryMonstersCover.jpg',
-  },
-  'album-3123123': {
-      id: 'album-3123123',
-      name: 'Low',
-      src: 'https://upload.wikimedia.org/wikipedia/ru/0/0e/DavidBowieScaryMonstersCover.jpg',
-  },
-  'album-41231293': {
-      id: 'album-41231293',
-      name: 'Lust for Life',
-      src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/7/72/IggyPopLustForLife.jpg/411px-IggyPopLustForLife.jpg',
-  },
-  'album-51230123': {
-      id: 'album-51230123',
-      name: 'Post Pop Depression',
-      src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/7/72/IggyPopLustForLife.jpg/411px-IggyPopLustForLife.jpg',
-  },
-  'album-6123123': {
-      id: 'album-6123123',
-      name: 'The Idiot',
-      src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/7/72/IggyPopLustForLife.jpg/411px-IggyPopLustForLife.jpg',
-  },
-  'album-7777777': {
-      id: 'album-7777777',
-      name: 'Transformer',
-      src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/f/f1/Loureedtransformer.jpeg/411px-Loureedtransformer.jpeg',
-  },
-  'album-8123123': {
-      id: 'album-8123123',
-      name: 'Berlin',
-      src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/f/f1/Loureedtransformer.jpeg/411px-Loureedtransformer.jpeg',
-  },
-  'album-9123123': {
-      id: 'album-9123123',
-      name: 'New York',
-      src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/f/f1/Loureedtransformer.jpeg/411px-Loureedtransformer.jpeg',
-  },
-  'album-100012301203': {
-      id: 'album-100012301203',
-      name: '45',
-      src: 'https://avatars.yandex.net/get-music-content/98892/985319cc.a.10562-3/m1000x1000',
-  },
   'album-11123123': {
-      id: 'album-11123123',
+    id: 'album-11123123',
+    data: {
       name: '46',
       src: 'https://avatars.yandex.net/get-music-content/98892/985319cc.a.10562-3/m1000x1000',
+    }
   },
-  'album-12121212': {
-      id: 'album-12121212',
-      name: 'Черный Альбом',
-      src: 'https://avatars.yandex.net/get-music-content/98892/985319cc.a.10562-3/m1000x1000',
-  }
 }
+
+// export const AlbumsDebugData : IAlbumsCollectionData = {
+//   'album-1123123': {
+//       id: 'album-1123123',
+//       name: 'Scary Monsters and Super Creeps',
+//       src: 'https://upload.wikimedia.org/wikipedia/ru/0/0e/DavidBowieScaryMonstersCover.jpg',
+//   },
+//   'album-212312': {
+//       id: 'album-212312',
+//       name: 'The Next Day',
+//       src: 'https://upload.wikimedia.org/wikipedia/ru/0/0e/DavidBowieScaryMonstersCover.jpg',
+//   },
+//   'album-3123123': {
+//       id: 'album-3123123',
+//       name: 'Low',
+//       src: 'https://upload.wikimedia.org/wikipedia/ru/0/0e/DavidBowieScaryMonstersCover.jpg',
+//   },
+//   'album-41231293': {
+//       id: 'album-41231293',
+//       name: 'Lust for Life',
+//       src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/7/72/IggyPopLustForLife.jpg/411px-IggyPopLustForLife.jpg',
+//   },
+//   'album-51230123': {
+//       id: 'album-51230123',
+//       name: 'Post Pop Depression',
+//       src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/7/72/IggyPopLustForLife.jpg/411px-IggyPopLustForLife.jpg',
+//   },
+//   'album-6123123': {
+//       id: 'album-6123123',
+//       name: 'The Idiot',
+//       src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/7/72/IggyPopLustForLife.jpg/411px-IggyPopLustForLife.jpg',
+//   },
+//   'album-7777777': {
+//       id: 'album-7777777',
+//       name: 'Transformer',
+//       src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/f/f1/Loureedtransformer.jpeg/411px-Loureedtransformer.jpeg',
+//   },
+//   'album-8123123': {
+//       id: 'album-8123123',
+//       name: 'Berlin',
+//       src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/f/f1/Loureedtransformer.jpeg/411px-Loureedtransformer.jpeg',
+//   },
+//   'album-9123123': {
+//       id: 'album-9123123',
+//       name: 'New York',
+//       src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/f/f1/Loureedtransformer.jpeg/411px-Loureedtransformer.jpeg',
+//   },
+//   'album-100012301203': {
+//       id: 'album-100012301203',
+//       name: '45',
+//       src: 'https://avatars.yandex.net/get-music-content/98892/985319cc.a.10562-3/m1000x1000',
+//   },
+//   'album-11123123': {
+//       id: 'album-11123123',
+//       data: {
+//         name: '46',
+//         src: 'https://avatars.yandex.net/get-music-content/98892/985319cc.a.10562-3/m1000x1000',
+//       }
+//   },
+//   'album-12121212': {
+//       id: 'album-12121212',
+//       name: 'Черный Альбом',
+//       src: 'https://avatars.yandex.net/get-music-content/98892/985319cc.a.10562-3/m1000x1000',
+//   }
+// }
